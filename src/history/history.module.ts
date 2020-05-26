ceef66ac765d81eea8206c425c8f5a6d57ddd0a9
wayne900619 <wayne900619@gmail.com>
2020-05-26 20:58:59 +0800

    miss chatroomsModule in historyModule

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -3,10 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { HistorySchema } from './history.schema';
 import { HistoryService } from './history.service';
 import { HistoryController } from './history.controller';
+import { ChatRoomModule } from 'src/chatrooms/chatrooms.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'History', schema: HistorySchema }]),
+    ,
+    ChatRoomModule,
   ],
   controllers: [HistoryController],
   providers: [HistoryService],
