015bd2658c8be4b7f8850eb0c4482c00b24f91af
wayne900619 <wayne900619@gmail.com>
2020-06-04 21:04:52 +0800

    update authorization headter to Authorization

--- src/auth/authorization.decorator.ts
+++ src/auth/authorization.decorator.ts
@@ -8,11 +8,11 @@ export const Authorization = createParamDecorator(
   (_, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     try {
-      request.headers['authorization'];
+      request.headers['Authorization'];
     } catch (e) {
       return null;
     }
-    const matches = request.headers['authorization'].match(/(\S+)\s+(\S+)/);
+    const matches = request.headers['Authorization'].match(/(\S+)\s+(\S+)/);
     if (!matches || matches[1] !== 'bearer') {
       return null;
     }
