feccd9b1d3d0300e7162a8825ab4847c04c94c94
wayne900619 <wayne900619@gmail.com>
2020-06-07 18:00:47 +0800

    fix .env bug

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.ProductionPort || 3000);
+  await app.listen(Number(process.env.ProductionPort) || 3000);
 }
 bootstrap();
