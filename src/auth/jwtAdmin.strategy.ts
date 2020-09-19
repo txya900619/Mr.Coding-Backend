b862c10c00c95d42b5988673f8cf2dcc24f7e276
txya900619 <wayne900619@gmail.com>
2020-09-19 13:38:51 +0800

    add jwtAdmin authGuard

---
+++ src/auth/jwtAdmin.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwtAdmin') {
+  constructor(private usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JwtSecret || 'cc',
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.usersService.findOneByID(payload.sub);
+    if (!user.admin) {
+      throw new UnauthorizedException();
+    }
+    return { _id: payload.sub, username: payload.username };
+  }
+}
