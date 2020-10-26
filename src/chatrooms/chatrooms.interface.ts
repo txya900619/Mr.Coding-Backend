80ffe92c1d3b986f69babedd3f6aedd61835cea6
txya900619 <wayne900619@gmail.com>
2020-10-27 02:13:06 +0800

    add name to chatroom interface

--- src/chatrooms/chatrooms.interface.ts
+++ src/chatrooms/chatrooms.interface.ts
@@ -2,6 +2,7 @@ import { Document } from 'mongoose';
 
 export interface ChatRoom extends Document {
   readonly _id: string;
+  readonly name: string;
   readonly lineChatroomUserID: string;
   readonly liffUserID: string;
   readonly closed: boolean;
